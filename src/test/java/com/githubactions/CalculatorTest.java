db573c11f4d624e85800dd346bc4e3cc604094ae
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2025-12-04 14:30:32 -0300

    Testing ci

--- src/test/java/com/githubactions/CalculatorTest.java
+++ src/test/java/com/githubactions/CalculatorTest.java
@@ -8,6 +8,6 @@ public class CalculatorTest {
     @Test
     public void sum_two_numbers_test(){
         int total = new Calculator().sum(1,2);
-        Assert.assertEquals(5, total);
+        Assert.assertEquals(6, total);
     }
 }
